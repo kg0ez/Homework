e0a9d4993be11a29b8e5d38e3a0cc08c876ce1f5
kg0ez <82456927+kg0ez@users.noreply.github.com>
2022-07-31 21:12:05 +0300

    Parser

--- WebAPI--Homework/SocialNetwork.Parser/Program.cs
+++ WebAPI--Homework/SocialNetwork.Parser/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using SocialNetwork.Parser.Services.Implementations;
+using SocialNetwork.Parser.Services.Interfaces;
 
+namespace SocialNetwork.Parser
+{
+    class Program
+    {
+        public static List<IArticle> articlesList = new List<IArticle>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private static async Task ChukotkaLoad()
+        {
+            string site = "https://old.prochukotku.ru";
+            WebRequest NewsListRequest = WebRequest.Create(site + "/news/main/");
+            using WebResponse NewsListResponse = await NewsListRequest.GetResponseAsync();
+            using Stream stream = NewsListResponse.GetResponseStream();
+            HtmlDocument document = new HtmlDocument();
+            document.Load(stream);
+
+            var NewsBlockNodes = document.DocumentNode.SelectNodes(".//div[contains(@class, 'item nuclear')]");
+            for (int i = 0; i < 10; i++)
+            {
+                IArticle article = new ChukotkaArticle();
+                var NewsNodes = NewsBlockNodes[i].SelectNodes(".//a");
+                article.FillParams(NewsNodes[0]);
+                articlesList.Add(article);
+            }
+        }
+        public static List<IArticle> Posts()
+        {
+            ChukotkaLoad();
+            return articlesList;
+        }
+    }
+}
---
+++ WebAPI--Homework/SocialNetwork.Parser/Services/Implementations/ChukotkaArticle.cs
@@ -0,0 +1,16 @@
+﻿using HtmlAgilityPack;
+using SocialNetwork.Parser.Services.Interfaces;
+
+namespace SocialNetwork.Parser.Services.Implementations
+{
+	public class ChukotkaArticle:IArticle
+	{
+        public string Name { get; set; }
+
+        public void FillParams(HtmlNode Node)
+        {
+            Name = Node.InnerText;
+        }
+    }
+}
+
